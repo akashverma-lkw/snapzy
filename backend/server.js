94ad0bcaadefd0152973af165785f35cd8085931
Akash Verma <akashvermastp7@gmail.com>
2025-07-05 20:00:42 +0530

    update files

--- backend/server.js
+++ backend/server.js
@@ -53,14 +53,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
-
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 	connectMongoDB();
