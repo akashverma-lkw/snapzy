e76d87774523b998afdcc286f5e5108e46cd24c2
Akash Verma <akashvermastp7@gmail.com>
2025-07-07 15:03:46 +0530

    update files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,31 +1,23 @@
 import daisyui from "daisyui";
-
+import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: [
-      "light", // built-in light theme
-      {
-        mycustom: {
-          primary: "#1d9bf0",    // custom blue
-          secondary: "#181818",  // dark secondary
-          accent: "#37cdbe",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
-          info: "#2094f3",
-          success: "#009485",
-          warning: "#ff9900",
-          error: "#ff5724",
-        },
-      },
-    ],
-  },
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+
+	daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
 };
